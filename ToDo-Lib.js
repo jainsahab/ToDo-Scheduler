c9cef5bf430b45faeb735b192d24a82f7b04861d
Prateek Jain <prateekj@thoughtworks.com>
2014-04-21 23:15:20 +0530

    functionality to prevent duplicate task has been done

--- ToDo-Lib.js
+++ ToDo-Lib.js
@@ -1,12 +1,24 @@
 var fs = require('fs');
-var tasks = fs.existsSync('tasks.json') 
-			? JSON.parse(fs.readFileSync('tasks.json','utf-8')) 
-			: {completed:[],remaining:[]}
 var lib = {};
 
 lib.addTask = function(task) {
-	tasks.remaining.push(task);
-	fs.writeFileSync('tasks.json',JSON.stringify(tasks));
+	var existingTasks = fs.existsSync('tasks.json') 
+			? JSON.parse(fs.readFileSync('tasks.json','utf-8')) 
+			: {completed:[],remaining:[]}
+	if(lib.exists(existingTasks.remaining,task))
+		return {msg:'Tasks Already Exists'}
+	existingTasks.remaining.push(task);
+	fs.writeFileSync('tasks.json',JSON.stringify(existingTasks));
+}
+
+lib.exists = function (existingTasks, taskToAdd) {
+	var exists = false;
+	var checkIfExist = function(singleTask){
+		if(singleTask.work == taskToAdd.work)
+			exists = true;
+	}
+	existingTasks.forEach(checkIfExist)
+	return exists;
 }
 
 exports.fs = fs;
--- ToDo-LibTest.js
+++ ToDo-LibTest.js
@@ -1,6 +1,7 @@
-var lib = require('./ToDo-Lib.js').lib;
 var assert = require('assert');
+var lib = require('./ToDo-Lib.js').lib;
 var fs = require('./ToDo-Lib.js').fs;
+
 var writtenData ;
 var test = {};
 
@@ -8,11 +9,49 @@ fs.writeFileSync = function(fileName,data){
 	writtenData = data;
 }
 
-test.shouldAddTheTaskIntoList = function () {
+fs.readFileSync = function(fileName) {
+	return writtenData;
+}
+
+test.should_Add_The_Task_Into_List = function () {
 	var task = {work:'read some book'};
-	lib.addTask(task);
 	var expectedResult = {remaining:[{work:'read some book'}],completed:[]};
+
+	lib.addTask(task);
+
 	assert.deepEqual(expectedResult,JSON.parse(writtenData));
 }
 
+test.should_not_add_the_task_when_the_same_task_exists = function () {
+	var errorMessage = {msg:'Tasks Already Exists'};
+	var task_1 = {work:'read some book'};
+	var task_2 = {work:'read some book'};
+
+	lib.addTask(task_1);
+	fs.existsSync = function() {return true;}
+	
+	var expectedMessage = lib.addTask(task_2);
+
+	assert.deepEqual(expectedMessage,errorMessage);
+}
+
+test.should_return_true_when_the_task_already_exists = function () {
+	var existingTasks = [{work:'read some book'}];
+	var task_2 = {work:'read some book'};
+
+	var answer	= lib.exists(existingTasks,task_2);
+
+	assert.ok(answer);
+}
+
+
+test.should_return_false_when_the_task_not_exists = function () {
+	var existingTasks = [];
+	var task_2 = {work:'read some book'};
+
+	var answer	= lib.exists(existingTasks,task_2);
+
+	assert.equal(answer,false);
+}
+ 
 exports.test = test;
