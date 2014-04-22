9dc6665433399b6d5c3456e799886d69ca67dc33
Prateek Jain <prateekj@thoughtworks.com>
2014-04-23 04:57:55 +0530

    scripts files has been moved to their respective location

---
+++ scripts/myone/ToDo-Lib.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var lib = {};
+
+var getExistingTasks = function() {
+	return fs.existsSync('tasks.json') 
+			? JSON.parse(fs.readFileSync('tasks.json','utf-8')) 
+			: {completed:[],remaining:[],totalTaskGenerated:0}
+}
+
+lib.addTask = function(work) {
+	var existingTasks = getExistingTasks();
+	if(lib.exists(existingTasks.remaining,work))
+		return {msg:'Tasks Already Exists'}
+	var task = {work:work};
+	task.id = ++existingTasks.totalTaskGenerated;
+	existingTasks.remaining.push(task);
+	fs.writeFileSync('tasks.json',JSON.stringify(existingTasks));
+}
+
+lib.exists = function (existingTasks, taskToAdd) {
+	var exists = false;
+	var checkIfExist = function(singleTask){
+		if(singleTask.work == taskToAdd)
+			exists = true;
+	}
+	existingTasks.forEach(checkIfExist)
+	return exists;
+}
+
+lib.getIndexOfElement = function (remainingTasks,id) {
+	var index = 0;
+	for (var i = 0; i < remainingTasks.length; i++) {
+		var task = remainingTasks[i];
+		if(task.id == id)
+			return index;
+		index++;
+	};
+}
+
+lib.moveToDone = function(id) {
+	var existingTasks = getExistingTasks();
+	var indexOfTaskCompleted = lib.getIndexOfElement(existingTasks.remaining,id);
+
+	existingTasks.completed.push(existingTasks.remaining[indexOfTaskCompleted]);
+	existingTasks.remaining.splice(indexOfTaskCompleted,1);
+
+	fs.writeFileSync('tasks.json',JSON.stringify(existingTasks));
+}
+exports.fs = fs;
+exports.lib = lib;
---
+++ scripts/myone/ToDo-LibTest.js
@@ -0,0 +1,88 @@
+var assert = require('assert');
+var lib = require('./ToDo-Lib.js').lib;
+var fs = require('./ToDo-Lib.js').fs;
+
+var writtenData ;
+var test = {};
+
+fs.writeFileSync = function(fileName,data){
+	writtenData = data;
+}
+
+fs.readFileSync = function(fileName) {
+	return writtenData;
+}
+
+test.should_Add_The_Task_Into_List = function () {
+	var task = 'read some book';
+	var expectedResult = {remaining:[{work:'read some book',id:1}],completed:[],totalTaskGenerated:1};
+
+	lib.addTask(task);
+
+	assert.deepEqual(expectedResult,JSON.parse(writtenData));
+}
+
+test.should_not_add_the_task_when_the_same_task_exists = function () {
+	var errorMessage = {msg:'Tasks Already Exists'};
+	var task_1 = 'read some book';
+	var task_2 = 'read some book';
+
+	lib.addTask(task_1);
+	fs.existsSync = function() {return true;}
+	
+	var expectedMessage = lib.addTask(task_2);
+
+	assert.deepEqual(expectedMessage,errorMessage);
+}
+
+test.should_return_true_when_the_task_already_exists = function () {
+	var existingTasks = [{work:'read some book'}];
+	var task_2 = 'read some book';
+
+	var answer	= lib.exists(existingTasks,task_2);
+
+	assert.ok(answer);
+}
+
+
+test.should_return_false_when_the_task_not_exists = function () {
+	var existingTasks = [];
+	var task_2 = 'read some book';
+
+	var answer	= lib.exists(existingTasks,task_2);
+
+	assert.equal(answer,false);
+}
+
+test.should_update_the_totalTaskGenerated_as_a_task_added_to_the_scheduler = function() {
+	var task_1 = 'read some book';
+	var task_2 = 'play Cricket';
+	lib.addTask(task_1);
+	lib.addTask(task_2);
+	var expectedResult = {remaining:[{work:'read some book',id:1},{work:'play Cricket',id:2}],completed:[],totalTaskGenerated:2};
+	
+	assert.deepEqual(expectedResult,JSON.parse(writtenData));
+}
+
+test.should_return_the_right_index_of_task_when_id_provided = function() {
+	var tasks = [{work:'read some book',id:2},{work:'play Cricket',id:1}]
+	var idToSearch = 1;
+	var expectedIndex = 1;
+
+	var actualIndex = lib.getIndexOfElement(tasks,idToSearch);
+
+	assert.equal(actualIndex,expectedIndex);
+}
+ 
+test.should_move_the_task_to_the_completed_stage = function() {
+	var task_1 = 'read some book';
+	var task_2 = 'play Cricket';
+	lib.addTask(task_1);
+	lib.addTask(task_2);
+	var expectedResult = {remaining:[{work:'play Cricket',id:2}],completed:[{work:'read some book',id:1}],totalTaskGenerated:2};
+	
+	lib.moveToDone(1);
+
+	assert.deepEqual(expectedResult,JSON.parse(writtenData));
+}
+exports.test = test;
